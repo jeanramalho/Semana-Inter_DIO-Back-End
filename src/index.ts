c0deace141fc63a9a5b3fab0b6f52c1a9bf73453
jeanramalho <jeanramalho.dev@gmail.com>
2021-12-08 17:52:12 -0300

    Cria funcionalidades iniciais projeto

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3300
 
-app.get('/', (req, res) => res.send('Express =  TypeScript Server'))
+import 'express-async-errors';
+import express from 'express';
+import { createConnection } from 'typeorm';
+import cors from 'cors';
 
-app.listen(port, () => {
-    console.log(`[server]: Server is runing at http://localhost:${port}`)
-})
+import {globalErrors} from './middlewares/globalErros'
+import routes from './routes';
+
+createConnection().then(connection => {
+  const app = express();
+  const PORT = 3333;
+  
+  app.use(cors())
+  app.use(express.json())
+  app.use(routes);
+
+  app.use(globalErrors);
+  
+  app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  });
+}).catch((error) => {
+  console.log("Unable to connect to the database", error)
+});
